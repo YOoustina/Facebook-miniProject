bd725b6c553cb8ecb8608cb73e78cf933facbbbb
YoustinaNader <youstinanader5@gmail.com>
2020-09-01 02:24:13 +0200

    change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from '../app/material.module';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostComponentComponent } from './post-component/post-component.component';
+
 // import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule,MaterialModule, AppRoutingModule,  ],
+  declarations: [AppComponent, PostComponentComponent],
+  imports: [BrowserModule, MaterialModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
